235035a2b8a949af361c0086b4cd480abeba9e23
Kim Lengen Gesite <kimlengel0408@gmail.com>
2026-06-24 22:39:18 +1000

    third attempt failed! Retrial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send(`Hello World! and this is me just testing it out! ${process.env.ENV}`);
+  res.send(`Hello World! and this is me just testing it out! Just adding this! ${process.env.ENV}`);
 });
 
 app.listen(port, () => {
